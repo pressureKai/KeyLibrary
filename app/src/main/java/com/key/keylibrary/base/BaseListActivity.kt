03e1edb84f088f321956438f6fdf99cf797384b1
kai <2466129279@qq.com>
2019-12-14 17:43:40 +0800

    change baseListActivity root view to linearlayout

--- app/src/main/java/com/key/keylibrary/base/BaseListActivity.kt
+++ app/src/main/java/com/key/keylibrary/base/BaseListActivity.kt
@@ -1,6 +1,8 @@
 package com.key.keylibrary.base
 
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.key.keylibrary.R
+import com.key.keylibrary.utils.UiUtils
 import com.key.keylibrary.widget.Toolbar
 import com.key.keylibrary.widget.recyclerview.KeyRecyclerView
 import com.scwang.smartrefresh.layout.SmartRefreshLayout
@@ -12,7 +14,6 @@ import me.jessyan.autosize.internal.CustomAdapt
 abstract class BaseListActivity<T> : BaseActivity(), CustomAdapt {
     var mToolbar: Toolbar? = null
     var mListView: KeyRecyclerView<T>? = null
-    var mRefreshOut :SmartRefreshLayout ?= null
     override fun setLayoutId(): Int {
         return R.layout.activity_base_list
     }
@@ -21,17 +22,10 @@ abstract class BaseListActivity<T> : BaseActivity(), CustomAdapt {
         setStateBarColor(R.color.blue, false)
         mToolbar = findViewById(R.id.toolbar)
         mListView = findViewById(R.id.list)
-        mRefreshOut = findViewById(R.id.refresh_out)
-        mRefreshOut!!.setEnableRefresh(false)
-        mRefreshOut!!.setEnableLoadMore(false)
+        val i = UiUtils.measureView(mToolbar)[1] + UiUtils.getStateBar(this)
+        mListView!!.setPadding(0,0,0, i)
     }
 
 
 
-    fun refreshEnable(enable : Boolean){
-        mRefreshOut!!.setEnableRefresh(enable)
-    }
-    fun loadMoreEnable(enable :Boolean){
-        mRefreshOut!!.setEnableLoadMore(enable)
-    }
 }
--- app/src/main/java/com/key/keylibrary/utils/UiUtils.java
+++ app/src/main/java/com/key/keylibrary/utils/UiUtils.java
@@ -3,6 +3,7 @@ package com.key.keylibrary.utils;
 import android.content.Context;
 import android.util.DisplayMetrics;
 import android.view.View;
+import android.view.ViewGroup;
 
 import static com.key.keylibrary.base.GlobalApplication.getContext;
 
@@ -45,5 +46,52 @@ public class UiUtils {
     public static int dip2px(float dip) {
         float density = getContext().getResources().getDisplayMetrics().density;
         return (int) (dip * density + 0.5f);
+    }
+
+
+    /**
+     * 获取某个控件的宽高
+     *
+     * @param view
+     * @return
+     */
+    public static int[] measureView(final View view) {
+        ViewGroup.LayoutParams lp = view.getLayoutParams();
+        if (lp == null) {
+            lp = new ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT
+            );
+        }
+        int widthSpec = ViewGroup.getChildMeasureSpec(0, 0, lp.width);
+        int lpHeight = lp.height;
+        int heightSpec;
+        if (lpHeight > 0) {
+            heightSpec = View.MeasureSpec.makeMeasureSpec(lpHeight, View.MeasureSpec.EXACTLY);
+        } else {
+            heightSpec = View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED);
+        }
+        view.measure(widthSpec, heightSpec);
+        return new int[]{view.getMeasuredWidth(), view.getMeasuredHeight()};
+    }
+
+
+
+
+
+    /**
+     * 获取状态栏高度
+     *
+     * @param context
+     * @return
+     */
+
+    public static int getStateBar(Context context) {
+        int result = 0;
+        int resourceId = context.getResources().getIdentifier("status_bar_height", "dimen", "android");
+        if (resourceId > 0) {
+            result = context.getResources().getDimensionPixelSize(resourceId);
+        }
+        return result;
     }
 }
